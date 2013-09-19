11cc8d419b3c3eb04d87e3e134e515a068b18cf1
Robert Taraya <robert.taraya@gmail.com>
2013-09-18 23:41:38 -0700

    create migration create_posts

---
+++ db/migrate/20130919054609_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration
+  def up
+    create_table :posts do |t|
+      t.string :title
+      t.string :description
+      t.string :price
+      t.email :email
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :posts
+  end
+end
